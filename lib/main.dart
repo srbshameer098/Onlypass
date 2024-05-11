85659028cbc023bc9789692a6df876071709ea03
srbshameer098 <srbshameer098@gmail.com>
2024-05-11 13:57:44 +0530

    firebase_login

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:onlypass/UI/Bottomnav.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:onlypass/bloc/Fecilities/fecilities_bloc.dart';
 
@@ -29,7 +29,13 @@ class ThemeProvider with ChangeNotifier {
     notifyListeners();
   }
 }
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+   
+
+  );
   runApp(const MyApp());
 }
 
